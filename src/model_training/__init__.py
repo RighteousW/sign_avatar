8f05adbf407dae09b97752996e112b686a0ff7bc
RighteousW <rjwasambo@gmail.com>
2025-11-01 10:59:48 +0200

    remove artifacts from gloss2text init file

--- src/model_training/__init__.py
+++ src/model_training/__init__.py
@@ -1,6 +1,4 @@
 from .gesture_recognizer import (
-    get_model_path as get_gesture_recognizer_model_path,
-    get_metadata_path as get_gesture_recognizer_metadata_path,
     GestureRecognizerModel,
 )
 from .gloss2text import (
@@ -8,25 +6,17 @@ from .gloss2text import (
     translate_sentence as gloss2text_translate_sentence,
     load_data_from_file,
     save_full_model,
-    GRUEncoder,
-    GRUDecoderWithAttention,
-    Seq2SeqWithAttention,
     build_vocab,
     load_checkpoint,
     save_full_model,
 )
 
 __all__ = [
-    "get_gesture_recognizer_model_path",
-    "get_gesture_recognizer_metadata_path",
     "GestureRecognizerModel",
     "load_gloss2text_full_model",
     "gloss2text_translate_sentence",
     "load_data_from_file",
     "save_full_model",
-    "GRUEncoder",
-    "GRUDecoderWithAttention",
-    "Seq2SeqWithAttention",
     "build_vocab",
     "load_checkpoint",
     "save_full_model",
